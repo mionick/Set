513b7ceeb5662c4ccce011a3b113262341fe7a57
mionick <mio.clause@gmail.com>
2018-06-03 15:05:21 -0400

    Working on multi player.
    At a turning point where I worked half way to put the canvas in a separate activity, and now decided that's a bad idea.
    
    committing in case that turns out the be the ONLY way.

--- app/src/main/java/io/github/mionick/set/android/MultiPlayerLobbyActivity.java
+++ app/src/main/java/io/github/mionick/set/android/MultiPlayerLobbyActivity.java
@@ -1,9 +1,21 @@
 package io.github.mionick.set.android;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import io.github.mionick.events.EventInstance;
+import io.github.mionick.set.IInputSource;
 import io.github.mionick.set.R;
+import io.github.mionick.set.RecordGenerator;
+import io.github.mionick.set.SetGame;
+import io.github.mionick.set.SetGameEvent;
+import io.github.mionick.set.web.ServerGameClient;
 
 /**
  * This Activity is meant as a stage where players can request to join the next game.
@@ -18,6 +30,10 @@ import io.github.mionick.set.R;
  */
 public class MultiPlayerLobbyActivity extends AppCompatActivity {
 
+    ServerGameClient webClient;
+    SetGame game;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,5 +42,35 @@ public class MultiPlayerLobbyActivity extends AppCompatActivity {
         // Start a web server thing?
         // I guess that will be running in the background.
         // Will have to see how the library works before I can really do anything here...
+        webClient = ServerGameClient.getInstance(getApplicationContext());
+
+        game = new SetGame();
+
+        // Register all input sources.
+        IInputSource[] sources = new IInputSource[]{
+                webClient
+        };
+
+        for (IInputSource source : sources) {
+            source.addSelectionEventHandler((sourceName, triple) -> game.SelectCards(sourceName, triple.toArray()));
+        }
+
+        game.eventHandlerSet.AddHandlerForAllEvents((timestamp, objects) -> {
+            // the gerneal handler takes object[]
+            // the only object in this array is the event that happened.
+            // It's designe dlike this because more specific handlers only receive params, not the event itself.
+            EventInstance<SetGameEvent> event = (EventInstance<SetGameEvent>) objects[0];
+            webClient.OnGameEvent(event);
+        });
+
     }
+
+    protected void StartGame(View v) {
+        Intent startGameIntent = new Intent(this, MultiPlayerGameActivity.class);
+        startGameIntent.put("game", this.game);
+        startActivity(startGameIntent);
+    }
+
+
+
 }
